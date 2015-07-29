794240ae079e6c830c7dcb002efd21f766824f0e
PRIESt512 <priest512@hotmail.com>
2015-07-29 20:06:32 +0300

    добавка

---
+++ WebCommandDevice/ControlDevice/RabbitMQ/Device.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using WebCommandDevice.ControlDevice.Pool;
+
+namespace WebCommandDevice.ControlDevice.RabbitMQ
+{
+    /// <summary>
+    /// Отправление и получение команд девайса
+    /// </summary>
+    /// <typeparam name="T">Реализация интерфейса для отправления или получения команд</typeparam>
+    public sealed partial class Device<T> : IReceiveCommand, ISenderCommand where T : class
+    {
+        private RabbitReceiner _receiner;
+        private RabbitSender _sender;
+
+        public Device(String deviceId)
+        {
+            if (typeof(T) == typeof(IReceiveCommand))
+                this._receiner = PoolConnection.RabbitReceiner.GetInstance(deviceId);
+            else
+                this._sender = PoolConnection.RabbitSender.GetInstance(deviceId);
+        }
+
+        /// <summary>
+        /// Выполняет возврат использованного соединения обратно в пул
+        /// </summary>
+        public void Dispose()
+        {
+            if (this._receiner != null) PoolConnection.RabbitReceiner.ReturnToPool(ref _receiner);
+            else PoolConnection.RabbitSender.ReturnToPool(ref _sender);
+
+        }
+    }
+}
